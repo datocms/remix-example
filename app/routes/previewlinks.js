9660ef0bf86777e3253e041139b3d2dcf56e4ae3
arcataroger <arcataroger@gmail.com>
2023-10-31 16:46:59 -0700

    Trying to fix previewlinks

--- app/routes/previewlinks.js
+++ app/routes/previewlinks.js
@@ -1,4 +1,24 @@
-export default function Serverless() {
+export default function Serverless(req) {
+
+
+    const baseUrl = process.env.VERCEL_URL
+        // Vercel auto-populates this environment variable
+        ? `https://${process.env.VERCEL_URL}`
+        // Netlify auto-populates this environment variable
+        : process.env.URL;
+
+
+    const generatePreviewUrl = ({item, itemType, locale}) => {
+        switch (itemType.attributes.api_key) {
+            case 'post':
+                return `/posts/${item.attributes.slug}`;
+            default:
+                return null;
+        }
+    }
+
+    const url = generatePreviewUrl(req.body);
+
     const previewLinks = [
         {
             label: 'Published version',
@@ -10,13 +30,14 @@ export default function Serverless() {
         },
     ]
 
-    return new Response(JSON.stringify({ previewLinks }), {
+    return new Response(JSON.stringify({previewLinks}), {
         status: 200,
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             'Content-Type': 'application/json',
+            'Content-Security-Policy': "frame-ancestors 'self' https://plugins-cdn.datocms.com"
         },
     })
 }
